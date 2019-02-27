c6c33791b6eaa7919b2ea857e2eb0c4e6df5d08b
Justin Abrahms <justin@abrah.ms>
2019-02-28 12:25:13 +1300

    slurp in the very first message

--- plan.md
+++ plan.md
@@ -4,10 +4,15 @@ The goal here is to provide a simple client-side frontend which calls the go rpc
 2. Patchwork-style "you have 4 new things. click to update"
 3. Initial page load fetches 10 posts.
 
-
 Open questions:
 - How do FE's authenticate with the backend such that they can get read access?
 - What is the correct API to get a post list?
 
 For later:
 - subscribe to websocket w/ a filter so we don't have to process all data.
+
+--
+
+1. [done] send cryptix a list of problems w/ onboarding
+2. figure out how to get author name (get post by type for 'about')
+3. figure out threadding
--- post_service.go
+++ post_service.go
@@ -1,6 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/pkg/errors"
+	"go.cryptoscope.co/luigi"
+	"go.cryptoscope.co/muxrpc"
+	"go.cryptoscope.co/ssb"
+	"go.cryptoscope.co/ssb/message"
+)
+
+type KeyValueRaw struct {
+	Key       *ssb.MessageRef       `json:"key"`
+	Value     message.LegacyMessage `json:"value"`
+	Timestamp int64                 `json:"timestamp"`
+}
 
 type Post struct {
 	Id, Body, Author string
@@ -17,29 +32,101 @@ type PostsListReply struct {
 type PostsService struct{}
 
 func (h *PostsService) List(r *http.Request, args *PostsListArgs, reply *PostsListReply) error {
-	reply.Posts = []Post{
-		Post{
-			Id:     "my-id",
-			Author: "someone",
-			Body:   "my body goes here",
-		},
-		Post{
-			Id:     "anotherId",
-			Author: "someone else",
-			Body:   "body text",
-			Children: &[]Post{
-				Post{
-					Id:     "first-child-id",
-					Author: "that first person",
-					Body:   "1st child",
-				},
-				Post{
-					Id:     "second-child-id",
-					Author: "that first person",
-					Body:   "2nd child",
-				},
-			},
-		},
+	fmt.Println("List method")
+	client, err := initClient("/home/justin/.ssb/secret") // this should be moved somewhere else
+	if err != nil {
+		// @@@ handle sending error reply
+		fmt.Println(err)
+		return err
 	}
+
+	fmt.Println("fetching from sbot")
+	// the map thing tells the go client what the marshal format is.
+	// src, err := client.Source(r.Context(), KeyValueRaw{}, muxrpc.Method{"createLogStream"})
+	src, err := client.Source(r.Context(), KeyValueRaw{}, muxrpc.Method{"messagesByType"}, "post")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	// we could call an API that's 'get post by type'
+
+	fmt.Println("iterating")
+	v, err := src.Next(r.Context())
+	if luigi.IsEOS(err) {
+		fmt.Println("eos")
+		return nil
+	} else if err != nil {
+		fmt.Println(err)
+		return errors.Wrapf(err, "createLogStream: failed to drain")
+	}
+	fmt.Printf("V: %#v\n\n", v)
+	msg := v.(KeyValueRaw)
+
+	fmt.Printf("Message: %#v\n\n", msg)
+	fmt.Printf("Author: %s\n\n", msg.Value.Author)
+	fmt.Printf("User's Timestamp: %d\n\n", msg.Value.Timestamp)
+	// lol @ content hash
+
+	content, ok := msg.Value.Content.(map[string]interface{})
+	if !ok {
+		fmt.Println("error swapping to content hash")
+		return fmt.Errorf("couldn't swap it to a content hash: %T", msg.Value.Content)
+	}
+
+	ctype, ok := content["type"]
+	if !ok {
+		fmt.Println("error type param")
+		return errors.New("Message didn't have a type param")
+	}
+	ctext, ok := content["text"]
+	if !ok {
+		fmt.Println("error text param")
+		return errors.New("Message didn't have text")
+	}
+	// croot, ok := content["root"]
+	// if !ok {
+	// 	return errors.New("Message didn't have a root param")
+	// }
+
+	switch ctype.(string) {
+	case "about":
+		fmt.Println("It's an about stanza")
+	case "post":
+		reply.Posts = []Post{
+			Post{
+				Id:     msg.Key.Ref(),
+				Author: msg.Value.Author,
+				Body:   ctext.(string),
+			},
+		}
+		// fmt.Println("It's an post stanza")
+	default:
+		fmt.Println("Unknown")
+	}
+
+	// reply.Posts = []Post{
+	// 	Post{
+	// 		Id:     "my-id",
+	// 		Author: "someone",
+	// 		Body:   "my body goes here",
+	// 	},
+	// 	Post{
+	// 		Id:     "anotherId",
+	// 		Author: "someone else",
+	// 		Body:   "body text",
+	// 		Children: &[]Post{
+	// 			Post{
+	// 				Id:     "first-child-id",
+	// 				Author: "that first person",
+	// 				Body:   "1st child",
+	// 			},
+	// 			Post{
+	// 				Id:     "second-child-id",
+	// 				Author: "that first person",
+	// 				Body:   "2nd child",
+	// 			},
+	// 		},
+	// 	},
+	// }
 	return nil
 }
